36ed0a4ec8a34c98c32b95899dde4a443c8c48e7
Tasha Mitchell <24355829+tamitchell@users.noreply.github.com>
2020-12-16 18:26:15 -0500

    removed security inhibitor

--- stylesheets/posthtml.config.js
+++ stylesheets/posthtml.config.js
@@ -2,7 +2,8 @@ module.exports = {
     plugins: {
       "posthtml-expressions": {
         locals: {
-          API_TOKEN: process.env.API_TOKEN
+          API_TOKEN: process.env.API_TOKEN,
+          COMPANY_EMAIL: process.env.COMPANY_EMAIL
         }
       }
     }
